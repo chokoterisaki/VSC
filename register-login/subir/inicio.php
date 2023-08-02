b9abb72f4735910807cfeeb117adb9951a9a291b
Maxi_mfg <118015781+Maxi2771@users.noreply.github.com>
2023-08-02 10:58:11 -0300

    Add files via upload

---
+++ register-login/subir/inicio.php
@@ -0,0 +1,115 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['usuario'])) {
+  echo '
+    <script>
+        alert("Debes iniciar sesion");
+        window.location = "../rp.php";
+    </script>
+    ';
+  session_destroy();
+  die();
+  session_destroy();
+}
+
+?>
+<?php include "../subir/db_conn.php"; ?>
+
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.84.0">
+  <title>Inicio</title>
+
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
+  <link rel="stylesheet" href="../assets/dist/css/style.css">
+
+
+
+  <!-- Bootstrap core CSS -->
+  <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+
+
+</head>
+
+<body>
+
+  <header>
+    <div class="collapse bg-dark" id="navbarHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-7 py-4">
+            <h4 class="text-white">About</h4>
+            <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+          </div>
+          <div class="col-sm-4 offset-md-1 py-4">
+            <h4 class="text-white">¿Qué puedo hacer?</h4>
+            <ul class="list-unstyled">
+              <li><a href="../cuenta.php" class="text-white">Cuenta</a></li>
+              <li><a href="index.php" class="text-white">Publicar</a></li>
+              <li><a href="./chat.php" class="text-white">Chat</a></li>
+              <li><a href="../php/cerrar_sesion.php" class="text-white">Cerrar Sesión</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+      <div class="container">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24">
+            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+            <circle cx="12" cy="13" r="4" />
+          </svg>
+          <strong>Home</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </div>
+  </header>
+  <main class="go">
+    <?php
+    $sql = "SELECT * FROM images ORDER BY id DESC";
+    $res = mysqli_query($conn,  $sql);
+
+    if (mysqli_num_rows($res) > 0) {
+      while ($images = mysqli_fetch_assoc($res)) {  ?>
+        <br>
+        <br>
+        <div class="alb">
+          <img src="uploads/<?= $images['image_url'] ?>">
+        </div>
+
+    <?php }
+    } ?>
+  </main>
+  <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
