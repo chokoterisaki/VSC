e49b194f611b22bf9b0e7aa0a39ce8749667e5ad
Maxi_mfg <118015781+Maxi2771@users.noreply.github.com>
2023-08-23 11:27:32 -0300

    Add files via upload

--- register-login/subir/index.php
+++ register-login/subir/index.php
@@ -1,31 +1,85 @@
 <!DOCTYPE html>
-<link rel="stylesheet" href="publicar.css">
 <html>
 
 <head>
 	<title>Subir imagen</title>
 	<style>
 		body {
+			background: rgb(35, 35, 37);
+			color: white;
 			display: flex;
 			justify-content: center;
 			align-items: center;
 			flex-direction: column;
 			min-height: 100vh;
 		}
+
+		form {
+			width: 550px;
+			height: 500px;
+			background: black;
+			border-radius: 50px;
+			border: 9px solid #7200be;
+			position: absolute;
+		}
+
+		.volver {
+			width: 50px;
+			height: 50px;
+			border-radius: 50px;
+			background-color: blueviolet;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border: 5px solid black;
+		}
+
+		.archivo {
+			margin-left: 80px;
+		}
+
+		.subir {
+			width: 100px;
+			height: 35px;
+			background: blue;
+			border-radius: 50px;
+			border: 5px solid white;
+			color: white;
+		}
+
+		.texto {
+			width: 450px;
+			height: 50px;
+			background-color: white;
+			border: 5px solid #7200be;
+
+		}
+		.objetos{
+			padding-top: 30%;
+			padding-left: 8%;
+		}
 	</style>
 </head>
 
 <body>
-	<a href="./inicio.php"><img src="flecha_atras.svg"></a>
 	<?php if (isset($_GET['error'])) : ?>
 		<p><?php echo $_GET['error']; ?></p>
 	<?php endif ?>
 	<form action="upload.php" method="post" enctype="multipart/form-data">
+		<div class="objetos">
+			<input type="file" name="my_image" class="archivo">
+			<input type="submit" name="submit" value="Subir" class="subir">
 
-		<input type="file" name="my_image" class="archivo">
-		<input type="submit" name="submit" value="Subir" class="subir">
-
+			<div class="volver">
+				<a href="./inicio.php"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-rounded-x" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M10 10l4 4m0 -4l-4 4" />
+						<path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
+					</svg></a>
+			</div>
+		</div>
 	</form>
+
 </body>
 
 </html>
--- register-login/subir/inicio.php
+++ register-login/subir/inicio.php
@@ -28,7 +28,6 @@ if (!isset($_SESSION['usuario'])) {
   <title>Inicio</title>
 
   <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
-  <link rel="stylesheet" href="../assets/dist/css/style.css">
 
 
 
@@ -49,14 +48,152 @@ if (!isset($_SESSION['usuario'])) {
         font-size: 3.5rem;
       }
     }
+
+    header {
+      width: 80px;
+      height: 100%;
+      position: fixed;
+      background-color: rgba(0, 0, 0, 0.80);
+    }
+
+    header a {
+      text-align: center;
+      display: block;
+      margin-bottom: 150px;
+      margin-left: 15px;
+      text-decoration: none;
+      color: white;
+      width: 50px;
+      height: 50px;
+      background-color: #4c00ff;
+      opacity: 0.7;
+      border-radius: 10px;
+    }
+
+    .separaciones {
+      text-align: center;
+      display: block;
+      margin-bottom: 150px;
+      margin-left: 15px;
+      text-decoration: none;
+      color: white;
+      width: 50px;
+      height: 50px;
+      background-color: #4c00ff;
+      opacity: 0.7;
+      border-radius: 10px;
+      margin-top: 20px;
+    }
+
+    .cuenta {
+      margin-top: 220px;
+
+    }
+
+    header img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50px;
+    }
+
+    .go {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin-left: 20px;
+    }
+
+    .go img {
+      width: 500px;
+      height: 500px;
+      border: 9px solid #4c00ff;
+    }
+
+    .texto {
+      width: 500px;
+      height: 50px;
+      border: 5px solid #4c00ff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .alb img {
+      width: 500px;
+      height: 250px;
+      border: 5px solid #4c00ff;
+    }
+
+    .go h2 {
+      font-size: medium;
+      color: black;
+    }
+
+    .mi-img {
+      position: fixed;
+      top: 400px;
+      left: 85px;
+      column-count: 3;
+      column-gap: 20px;
+      width: 600px;
+      height: 150px;
+      color: white;
+      background-color: grey;
+      border: 5px solid #4c00ff;
+    }
+
+    .mi-img label {
+      margin-bottom: 10px;
+      padding: 10px;
+    }
+
+    .mi-img input {
+      margin-bottom: 10px;
+      padding: 10px;
+    }
+
+    .mi-img button {
+      margin-bottom: 8px;
+      padding: 10px;
+    }
   </style>
 
 
 </head>
 
 <body>
-
   <header>
+    <div class="menu">
+      <img class="separaciones" src="../assets/images/VSCk_big.jpg" alt="">
+      <a class="botones separaciones" href="home"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+          <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+          <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
+        </svg></a>
+      <a class="botones separaciones" href="chat"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-message-code" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M8 9h8" />
+          <path d="M8 13h6" />
+          <path d="M11.012 19.193l-3.012 1.807v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v6" />
+          <path d="M20 21l2 -2l-2 -2" />
+          <path d="M17 17l-2 2l2 2" />
+        </svg></a>
+      <a class="botones separaciones" href="./upload.php"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 5l0 14" />
+          <path d="M5 12l14 0" />
+        </svg></a>
+      <a class="botones cuenta" href="cuenta"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+          <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+        </svg></a>
+    </div>
+  </header>
+
+  <!--<header>
     <div class="collapse bg-dark" id="navbarHeader">
       <div class="container">
         <div class="row">
@@ -90,22 +227,48 @@ if (!isset($_SESSION['usuario'])) {
         </button>
       </div>
     </div>
-  </header>
+  </header>-->
+  <br>
+  <form class="mi-img" action="upload.php" method="post" enctype="multipart/form-data">
+    <label for="nombre">Nombre:</label>
+    <input type="text" name="nombre" id="nombre" required>
+    <br>
+    <label for="imagen">Imagen:</label>
+    <input type="file" name="imagen" id="imagen" accept="image/*" required>
+    <br>
+    <button type="submit" name="submit">Subir</button>
+  </form>
   <main class="go">
     <?php
-    $sql = "SELECT * FROM images ORDER BY id DESC";
-    $res = mysqli_query($conn,  $sql);
+    // Mostrar todas las imágenes y nombres guardados en la base de datos
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "register_login_rp";
 
-    if (mysqli_num_rows($res) > 0) {
-      while ($images = mysqli_fetch_assoc($res)) {  ?>
-        <br>
-        <br>
-        <div class="alb">
-          <img src="uploads/<?= $images['image_url'] ?>">
-        </div>
+    // Crear conexión
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    <?php }
-    } ?>
+    // Verificar conexión
+    if ($conn->connect_error) {
+      die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    // Obtener información de la base de datos
+    $sql = "SELECT nombre, image_url FROM imagenes ORDER BY id DESC";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      while ($row = $result->fetch_assoc()) {
+        echo "<p class='texto'>" . $row['nombre'] . "</p>";
+        echo "<img src='" . $row['image_url'] . "' alt='Imagen'><br>";
+      }
+    } else {
+      echo "No se encontraron imágenes.";
+    }
+
+    // Cerrar conexión
+    ?>
   </main>
   <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
 
