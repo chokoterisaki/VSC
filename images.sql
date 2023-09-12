4b48a0ba28ad80d7c8a506c052d10eee98eff83e
chokoterisaki <axelrojogamer@gmail.com>
2023-09-11 21:30:19 -0300

    b

---
+++ images.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-08-2023 a las 21:07:53
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `register_login_rp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `image_url` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `images`
+--
+
+INSERT INTO `images` (`id`, `image_url`) VALUES
+(1, 'IMG-64dd1cc829ae79.63693088.jpg');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- register-login/subir/upload.php
+++ register-login/subir/upload.php
@@ -1,40 +1,47 @@
-<?php
-// Configuración de la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "register_login_rp";
+<?php 
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+if (isset($_POST['submit']) && isset($_FILES['my_image'])) {
+	include "db_conn.php";
 
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+	echo "<pre>";
+	print_r($_FILES['my_image']);
+	echo "</pre>";
+
+	$img_name = $_FILES['my_image']['name'];
+	$img_size = $_FILES['my_image']['size'];
+	$tmp_name = $_FILES['my_image']['tmp_name'];
+	$error = $_FILES['my_image']['error'];
+
+	if ($error === 0) {
+		if ($img_size > 100000000) {
+			$em = "Lo sentimos, su archivo es demasiado grande";
+		    header("Location: index.php?error=$em");
+		}else {
+			$img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+			$img_ex_lc = strtolower($img_ex);
+
+			$allowed_exs = array("jpg", "jpeg", "png", "zip", "gif"); 
+
+			if (in_array($img_ex_lc, $allowed_exs)) {
+				$new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
+				$img_upload_path = 'uploads/'.$new_img_name;
+				move_uploaded_file($tmp_name, $img_upload_path);
+
+				// Insert into Database
+				$sql = "INSERT INTO images(image_url) 
+				        VALUES('$new_img_name')";
+				mysqli_query($conn, $sql);
+				header("Location: inicio.php");
+			}else {
+				$em = "No puedes subir archivos de este tipo";
+		        header("Location: index.php?error=$em");
+			}
+		}
+	}else {
+		$em = "¡Ocurrió un error desconocido!";
+		header("Location: index.php?error=$em");
+	}
+
+}else {
+	header("Location: index.php");
 }
-
-// Procesar formulario si se ha enviado
-if (isset($_POST['submit'])) {
-    $nombre = $_POST['nombre'];
-    $imagenNombre = $_FILES['imagen']['name'];
-    $imagenTempPath = $_FILES['imagen']['tmp_name'];
-
-    // Mover imagen a una ubicación permanente
-    $imagenDestino = "uploads/" . $imagenNombre;
-    move_uploaded_file($imagenTempPath, $imagenDestino);
-
-    // Insertar información en la base de datos
-    $sql = "INSERT INTO imagenes (nombre, image_url) VALUES ('$nombre', '$imagenDestino')";
-    if ($conn->query($sql) === TRUE) {
-        // Redirigir de vuelta a index.html con los parámetros de información
-        header("Location: inicio.php?nombre=$nombre&image_url=$imagenDestino");
-        exit();
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
-
-// Redirigir de vuelta a index.html
-header("Location: inicio.php");
-?>
-
